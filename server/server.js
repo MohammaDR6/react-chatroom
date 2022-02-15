bb8ebdd693c0f3e3a076c715ee3b5b6d92ef36c9
MohammaDR6 <mohammadblack75@gmail.com>
2022-02-15 13:40:00 +0330

    basic part of server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const { Server } = require("socket.io")
+const cors = require('cors')
+
+app.use(cors())
+const server = http.createServer(app)
+
+server.listen(3001, () => {
+    console.log("Server is On");
+});
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (x)=> {
+    console.log(`User just connected : ${x.id}`);
+
+    x.on("joinRoom", (data)=> {
+        x.join(data);
+        console.log(`User with ${x.id} id joined ${data} Room`);
+    });
+});
