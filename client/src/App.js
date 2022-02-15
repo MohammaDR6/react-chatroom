54832cc5dc7a92d94a072bb87ff73d9ffdba5864
MohammaDR6 <mohammadblack75@gmail.com>
2022-02-15 14:05:42 +0330

    update components folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { useState } from 'react';
+import io from 'socket.io-client'
+import Joiningroom from './components/Joiningroom/Joiningroom'
+import Joiningroom from './components/Chatroom/Chatroom'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router >
+
+      <Switch>
+        <Route path="/joiningroom">
+          <Joiningroom />
+        </Route>
+      </Switch>
       
-    </div>
+      <Switch>
+        <Route path="/chatroom">
+          <Chatroom />
+        </Route>
+      </Switch>
+
+    </Router>
   );
 }
 
